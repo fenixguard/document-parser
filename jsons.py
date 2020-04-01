597c9569d2a48d242638e4c4300dd48c2e977eec
Sergey Volkov <fenixguard1993@gmail.com>
2020-04-01 10:17:29 +0300

    refactor code and merge function xml and json to one project

--- jsons.py
+++ jsons.py
@@ -1,7 +1,24 @@
 import json
+from xml.etree import ElementTree
 
 
-def create_wordlist(path):
+def create_wordlist_xml(path):
+    word_list = list()
+    tree = ElementTree.parse(path)
+    root = tree.getroot()
+    for child_element in root:
+        for child in child_element:
+            for sub_child in child:
+                if sub_child.tag == 'description':
+                    temp_list = list()
+                    temp_list.extend(sub_child.text.split())
+                    for t in temp_list:
+                        if len(t) > 6:
+                            word_list.append(t)
+    return word_list
+
+
+def create_wordlist_json(path):
     word_list = list()
     with open(path, "r", encoding="utf-8") as read_file:
         data = json.load(read_file)
@@ -34,8 +51,22 @@ def print_result():
         print(f"{stat[0]}: {stat[1]}")
 
 
+def main():
+    print("Выберите какой формат файла хотите обработать, поддерживаемые форматы json, xml")
+    print("-" * 40)
+    number = int(input("Если хотите обработать json - введите 1\nЕсли хотите обработать xml - введите 2\n"))
+    print('-' * 40)
+    print("Слово: количество повторений")
+    print('-' * 40)
+    assert number == 1 or number == 2, 'Вы ввели неверную цифру, введите 1 - json или 2 - xml.'
+    if number == 1:
+        return create_wordlist_json('newsafr.json')
+    if number == 2:
+        return create_wordlist_xml('newsafr.xml')
+
+
 if __name__ == '__main__':
-    word_list = create_wordlist('newsafr.json')
+    word_list = main()
     word_count = count_word()
     stats_sorted = sorted_word()
     print_result()
